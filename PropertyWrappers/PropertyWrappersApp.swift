ade54ea250a6573f97daf051d8d397020468fb63
Skripkin <artem.skripkin@magenta-technology.com>
2023-01-29 16:48:06 +0400

    initial commit

---
+++ PropertyWrappers/PropertyWrappersApp.swift
@@ -0,0 +1,17 @@
+//
+//  PropertyWrappersApp.swift
+//  PropertyWrappers
+//
+//  Created by Артём Скрипкин on 29.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct PropertyWrappersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
