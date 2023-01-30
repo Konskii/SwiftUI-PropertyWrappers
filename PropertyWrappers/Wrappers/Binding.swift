02fa2e8bc6705818071416557460344d737e6cbb
Skripkin <artem.skripkin@magenta-technology.com>
2023-01-30 11:10:54 +0400

    add BindingWrapper

--- PropertyWrappers/Wrappers/Binding.swift
+++ PropertyWrappers/Wrappers/Binding.swift
@@ -5,4 +5,35 @@
 //  Created by Артём Скрипкин on 29.01.2023.
 //
 
-import Foundation
+import SwiftUI
+
+struct BindingWrapper: View {
+
+    @State var showColor = false
+
+    var body: some View {
+        HStack {
+            Button("Show color") {
+                showColor = true
+            }
+        }
+        .sheet(isPresented: $showColor) {
+            ShowColorView(isShowed: $showColor)
+        }
+    }
+}
+
+private struct ShowColorView: View {
+    @Binding var isShowed: Bool
+
+    var body: some View {
+        VStack {
+            Color(.blue)
+                .overlay {
+                    Button("Dismiss") {
+                        isShowed = false
+                    }
+                }
+        }
+    }
+}
